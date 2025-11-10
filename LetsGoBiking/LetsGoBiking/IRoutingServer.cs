d7e35a4402da44c40272e256eda3739b44af1e2a
hamidamediaz <amediazhamid736@gmail.com>
2025-11-10 21:42:22 +0100

    set up project

---
+++ LetsGoBiking/LetsGoBiking/IRoutingServer.cs
@@ -0,0 +1,13 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace RoutingService
+{
+    [ServiceContract]
+    public interface IRoutingService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "/itinerary?origin={o}&destination={d}", ResponseFormat = WebMessageFormat.Json)]
+        string GetItinerary(string o, string d);
+    }
+}
